190e87044edc198e8fefd9339e93f39faa5180c5
Robert Jones <robert.ervin.jones@gmail.com>
2022-10-05 13:54:07 -0500

    lints web_blast

--- scripts/web_blast.py
+++ scripts/web_blast.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+'''
+BLASTS a sequence against online database and returns the top hit.
+'''
 
 import argparse
-from Bio.Blast import NCBIWWW, NCBIXML
-from Bio import SeqIO
 import multiprocessing as mp
 from functools import partial
-
+from Bio.Blast import NCBIWWW, NCBIXML
+from Bio import SeqIO
 
 def get_args():
+    '''
+    Set up command line arguments.
+
+    :return: parsed arguments
+    :rtype: argparse.Namespace
+    '''
     parser = argparse.ArgumentParser(description='Script for performing web BLAST Search',
-                                     usage="get_mito_genome.py -q QUERY -d DATABASE -b BLAST_TYPE [OPTIONS]",
+                                     usage="get_mito_genome.py -q QUERY -d DATABASE \
+                                         -b BLAST_TYPE [OPTIONS]",
                                      formatter_class=argparse.RawTextHelpFormatter)
     optional = parser._action_groups.pop()  # Edited this line
     required = parser.add_argument_group('required arguments')
@@ -35,8 +44,20 @@ def get_args():
     return parser.parse_args()
 
 
-def do_blast(seq, blast_type, db):
-    blast_results = NCBIWWW.qblast(program=blast_type, database=db, sequence=seq)
+def do_blast(seq, blast_type, database):
+    '''
+    Performs BLAST search.
+
+    :param seq: query sequence
+    :type seq: str
+    :param blast_type: blastn, blastp, blastx, tblastn, or tblastx
+    :type blast_type: str
+    :param database: wev database to be used
+    :type database: str
+    :return: blast record
+    :rtype: Bio.Blast.Record.Blast
+    '''
+    blast_results = NCBIWWW.qblast(program=blast_type, database=database, sequence=seq)
     return blast_results
     # with open("my_blast.xml", "a") as save_to:
     #     save_to.write(blast_results.read())
@@ -59,12 +80,14 @@ if __name__ == '__main__':
     records = SeqIO.parse(infile, format='fasta')
 
     with mp.Pool(processes=int(args.threads)) as p:
-        # Iterable to be used in p.map. This iterates through each sequence in the input fasta file.
+        # Iterable to be used in p.map.
+        # This iterates through each sequence in the input fasta file.
         _iter = [record.format(format='fasta') for record in records]
 
-        # first argument is function to be run in parallel. 2nd and 3rd arguments are function arguments that are the
-        # same for all iterations of the function
-        func = partial(do_blast, blast_type=args.blast_type, db=args.database)
+        # first argument is function to be run in parallel.
+        # 2nd and 3rd arguments are function arguments
+        # that are the same for all iterations of the function
+        func = partial(do_blast, blast_type=args.blast_type, database=args.database)
         results = p.map(func, iterable=_iter)
 
     for result in results:
